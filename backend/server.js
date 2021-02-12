f84e3cbbea6ae5be3ca42d27c6fea96dcfb7201d
Gardian <gardianii@hotmail.com>
2021-02-12 21:14:57 +0100

    Custom error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import color from 'colors'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +17,11 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+//Custom Error Handling
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
